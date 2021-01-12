09562c95dd7997d6ba4e05f6b3d2f6cc923b34c4
Arnaud Mandy <arnaud.mandy@intel.com>
2021-01-12 21:01:09 +0200

    gravity: adding gravity sensor
    Few debug message to page.
    
    Signed-off-by: Arnaud Mandy <arnaud.mandy@intel.com>

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var text = null;
-var acl = null;
 var startAcc = false;
+var startGravi = false;
 var startGyro = false;
+
+let acl = new Accelerometer({frequency:250});
+let gravi = new GravitySensor({frequency:60});
+let gyro = new Gyroscope({frequency:60});
 
 function setText(text) {
     game_text.innerText = text;
@@ -11,29 +15,46 @@ function setText(text) {
 function main() {
  game_text = document.getElementById("game_text");
  
-//acl = new LinearAccelerationSensor({frequency:250});
-acl = new Accelerometer({frequency:250});
-gyro = new Gyroscope({frequency:60});
+  //acl = new LinearAccelerationSensor({frequency:250});
+//  let acl = new Accelerometer({frequency:250});
+//  let gravi = new GravitySensor({frequency:60});
+//  let gyro = new Gyroscope({frequency:60});
 
-acl.start();
-startAcc = true;
+  //acl.start();
+  //startAcc = true;
 
-acl.addEventListener('reading', e => {
+  acl.addEventListener('reading', e => {
     document.getElementById("acc_x_val").innerHTML = Math.round(acl.x*1000)/1000;
     document.getElementById("acc_y_val").innerHTML = Math.round(acl.y*1000)/1000;
     document.getElementById("acc_z_val").innerHTML = Math.round(acl.z*1000)/1000;
   });
 
-gyro.addEventListener('reading', e => {
+  gravi.addEventListener('reading', e => {
+    document.getElementById("gravity_x_val").innerHTML = Math.round(gravi.x*1000)/1000;
+    document.getElementById("gravity_y_val").innerHTML = Math.round(gravi.y*1000)/1000;
+    document.getElementById("gravity_z_val").innerHTML = Math.round(gravi.z*1000)/1000;
+  });
+
+  gyro.addEventListener('reading', e => {
     document.getElementById("gyro_x_val").innerHTML = Math.round(gyro.x*1000)/1000;
     document.getElementById("gyro_y_val").innerHTML = Math.round(gyro.y*1000)/1000;
     document.getElementById("gyro_z_val").innerHTML = Math.round(gyro.z*1000)/1000;
   });
 
-acl.onerror = event => console.log(event.error.name, event.error.message);
-gyro.onerror = event => console.log(event.error.name, event.error.message);
-}
+  acl.onerror = event => {
+    console.log(event.error.name, event.error.message);
+    document.getElementById("acc_status").innerHTML = event.error.message;
+  }
+  gravi.onerror = event => {
+    console.log(event.error.name, event.error.message);
+    document.getElementById("gravity_status").innerHTML = event.error.message;
+  }
 
+  gyro.onerror = event => {
+    console.log(event.error.name, event.error.message);
+    document.getElementById("gyro_status").innerHTML = event.error.message;
+  }
+}
 function startStopAcc() {
     if (startAcc == false) {
         document.getElementById("accStatus").innerHTML = "ON";
@@ -46,6 +67,22 @@ function startStopAcc() {
         document.getElementById("acc_x_val").innerHTML = "-";
         document.getElementById("acc_y_val").innerHTML = "-";
         document.getElementById("acc_z_val").innerHTML = "-";
+        document.getElementById("acc_status").innerHTML = "";
+    }
+}
+function startStopGravity() {
+    if (startGravi == false) {
+        document.getElementById("gravityStatus").innerHTML = "ON";
+        startGravi = true;
+        gravi.start();
+    } else {
+        document.getElementById("gravityStatus").innerHTML = "OFF";
+        startGravi = false;
+        gravi.stop();
+        document.getElementById("gravity_x_val").innerHTML = "-";
+        document.getElementById("gravity_y_val").innerHTML = "-";
+        document.getElementById("gravity_z_val").innerHTML = "-";
+        document.getElementById("gravity_status").innerHTML = "";
     }
 }
 function startStopGyro() {
@@ -60,5 +97,6 @@ function startStopGyro() {
         document.getElementById("gyro_x_val").innerHTML = "-";
         document.getElementById("gyro_y_val").innerHTML = "-";
         document.getElementById("gyro_z_val").innerHTML = "-";
+        document.getElementById("gyro_status").innerHTML = "";
     }
 }
